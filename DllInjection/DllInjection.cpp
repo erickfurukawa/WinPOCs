c4b467eafbc4f938561024c7114f792b22c55066
Erick Furukawa <erickfurukawa1@gmail.com>
2023-01-05 20:29:51 -0300

    DllInjection accept dll relative path
    - Show error message when dll cannot be found
    - Convert dll relative path to full path before injection

--- DllInjection/DllInjection.cpp
+++ DllInjection/DllInjection.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "../Common/Constants.h"
 #include "../Common/Process.h"
 
@@ -7,18 +8,28 @@ bool injectDll(HANDLE hProc, char* dllPath);
 int main(int argc, char** argv)
 {
     char processName[MAX_LENGTH];
-    char dllPath[MAX_LENGTH];
+    char dllPath[MAX_PATH];
 
     if (argc == 3) {
         strncpy_s(processName, argv[1], MAX_LENGTH);
-        strncpy_s(dllPath, argv[2], MAX_LENGTH);
+        strncpy_s(dllPath, argv[2], MAX_PATH);
     }
     else {
         std::cout << "Process name:\n";
         std::cin.getline(processName, MAX_LENGTH);
         std::cout << "Dll path:\n";
-        std::cin.getline(dllPath, MAX_LENGTH);
+        std::cin.getline(dllPath, MAX_PATH);
     }
+
+    std::ifstream file;
+    file.open(dllPath);
+    if (!file) 
+    {
+        std::cerr << "Could not open dll " << dllPath << std::endl;
+        return 1;
+    }
+    file.close();
+    GetFullPathName(dllPath, MAX_PATH, dllPath, nullptr);
 
     std::cout << "Injecting dll " << dllPath <<  " into process " << processName << std::endl;
 
