94879dde2014e558aaf32370940c5ba9c23df81e
Erick Furukawa <erickfurukawa1@gmail.com>
2023-01-18 19:54:45 -0300

    create ScanPattern function

--- Common/Utils.cpp
+++ Common/Utils.cpp
@@ -1,4 +1,5 @@
 #include "Utils.h"
+#include <windows.h>
 #include <iostream>
 #include <fstream>
 
@@ -23,4 +24,29 @@ bool FileExists(char* fileName)
     }
     file.close();
     return true;
+}
+
+BYTE* ScanPattern(BYTE* pattern, char* mask, BYTE* src, uintptr_t srcSize)
+{
+    uintptr_t patternLen = strnlen(mask, srcSize);
+    for (uintptr_t i = 0; i < srcSize; i++)
+    {
+        bool found = true;
+        for (uintptr_t j = 0; j < patternLen; j++)
+        {
+            if (mask[j] != '?') // matches anything
+            {
+                if (pattern[j] != src[i + j])
+                {
+                    found = false;
+                    break;
+                }
+            }
+        }
+        if (found)
+        {
+            return src + i;
+        }
+    }
+    return nullptr;
 }
--- Common/Utils.h
+++ Common/Utils.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <windows.h>
 #include <string>
 
 void ThrowException(std::string msg);
@@ -7,3 +8,9 @@ void ThrowException(std::string msg);
 void ThrowException(char* msg);
 
 bool FileExists(char* fileName);
+
+/*
+    ScanPattern searches for the first ocurrence of "pattern" in "src".
+    The mask can contain '?' characters to match any byte.
+*/
+BYTE* ScanPattern(BYTE* pattern, char* mask, BYTE* src, uintptr_t srcSize);
