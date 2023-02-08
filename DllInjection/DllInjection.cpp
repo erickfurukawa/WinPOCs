b97141df361c0a73d1ba5d267bef7ab40b4cff4a
Erick Furukawa <erickfurukawa1@gmail.com>
2023-02-08 20:11:46 -0300

    get LoadLibraryA address from kernel32.dll file instead of GetProcAddress

--- DllInjection/DllInjection.cpp
+++ DllInjection/DllInjection.cpp
@@ -1,13 +1,19 @@
 #include "DllInjection.h"
+#include "../Common/PE.h"
 
 // The thread handle must be closed by the caller
 HANDLE InjectDll(Process* proc, char* dllPath) {
     void* dllPathAddr = proc->AllocMemory(strlen(dllPath) + 1);
     proc->WriteMemory(dllPathAddr, (BYTE*)dllPath, strlen(dllPath) + 1);
 
-    HMODULE kernel32 = GetModuleHandleA("kernel32.dll");
-    void* loadLibraryAddr = GetProcAddress(kernel32, "LoadLibraryA");
-
+    // find address of LoadLibraryA.
+    // needs to find base address of kernel32.dll and the function RVA
+    MODULEENTRY32 meKernel32 = proc->GetModule("kernel32.dll");
+    PE* kernel32 = new PE(meKernel32.szExePath);
+    DWORD loadLibraryRVA = kernel32->GetExportRVA("LoadLibraryA");
+    delete kernel32;
+    void* loadLibraryAddr = meKernel32.modBaseAddr + loadLibraryRVA;
+    
     HANDLE hThread = CreateRemoteThread(proc->handle, nullptr, 0, (LPTHREAD_START_ROUTINE)loadLibraryAddr, dllPathAddr, 0, nullptr);
     if (!hThread)
     {
